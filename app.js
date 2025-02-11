5e20bfbf77e0677646187a0f4577e4d068b75dc0
Viral169 <vv564577@gmail.com>
2025-02-11 23:01:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function bulb_on() {
+  let img = document.querySelector(".img");
+  img.setAttribute("src", "img/bulb-on.png");
+}
+function bulb_off() {
+  let img = document.querySelector(".img");
+  img.setAttribute("src", "img/bulb-off.png");
+}
